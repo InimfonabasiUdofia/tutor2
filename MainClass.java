720493181b7602cec1f2862d687c36a4c8eff826
inimfonabasi <inimfonabasi2323@gmail.com>
2026-05-19 13:34:30 -0700

    BODMAS

--- MainClass.java
+++ MainClass.java
@@ -1,16 +1,17 @@
+
 import java.util.*;
 
-public class MainClass{
-	public static void main(String args[])
-	{
-		Method method=new Method();
-		System.out.println("input your calculation");
-		Scanner sc =new Scanner(System.in);
-		String numbers=sc.nextLine();
-		
-		Double total=method.calculate(numbers);
-		System.out.println(total);
+public class MainClass {
 
-		sc.close();
-	}
+    public static void main(String args[]) {
+   
+
+        Method method=new Method();
+        System.out.println("input your calculation");
+        Scanner sc =new Scanner(System.in);
+        String numbers=sc.nextLine();
+        LinkedList<Double> total=method.calculate(numbers);
+        System.out.println(total);
+        sc.close();
+    }
 }
--- Method.java
+++ Method.java
@@ -1,28 +1,107 @@
-interface Method2 {
-	   double calculate( String numbers);
-}
-public class Method implements Method2{
 
- public double calculate(String numbers) {
-       String[] tokens = numbers.split("(?<=\\d)(?=[+\\-*/])|(?<=[+\\-*/])(?=\\d)");
+import java.util.LinkedList;
 
-        double result = Double.parseDouble(tokens[0]);
+public class Method {
 
-        for (int j = 1; j < tokens.length - 1; j += 2) {
-            double next = Double.parseDouble(tokens[j + 1]);
+    /**
+     * @param
+     * console calculator that obeys rule of BODMAS
+     * 
+     */
+    
+    public LinkedList<Double> calculate(String expression) {
 
-            if (tokens[j].equals("+")) {
-                result =result+ next;
-            } else if (tokens[j].equals("-")) {
-                result =result- next;
-            } else if (tokens[j].equals("*")) {
-                result =result*  next;
-            } else if (tokens[j].equals("/")) {
-                result =result/ next;
+        LinkedList<Double> List_operand = new LinkedList<>();
+        LinkedList<String> List_operator = new LinkedList<>();
+
+
+        /* * 
+        * Split the expression into operands and operators
+        * then store them in a seperate array
+        */
+        String[] operands = expression.split("[+\\-*/^%]");
+        String[] operator = expression.trim().replaceAll("^[0-9.]+", "").split("[0-9.\\s]+");
+
+
+        /* *
+        * use a for loop to store the array operands and operators
+        * in a seperate linkedlist
+        */
+        for (int i = 0; i < operands.length; i++) {
+            List_operand.add(Double.parseDouble(operands[i]));
+        }
+        for (int i = 0; i < operator.length; i++) {
+            List_operator.add(operator[i]);
+        }
+
+
+        /**
+         *  Resolve all division and multiplication  operations from left to right.
+         * Iterates through the operator list and finds / and * operators,
+         * computes the result, and updates both lists accordingly .
+         */
+        for (int i = 0; i < List_operator.size(); i++) {
+            if (List_operator.get(i).equals("/") ||List_operator.get(i).equals("*")) {
+                double left = List_operand.get(i);
+                double right = List_operand.get(i + 1);
+                double result;
+                if (List_operator.get(i).equals("/")){
+                     result = left / right;
+                }else{
+                     result = left * right;
+                }
+
+                List_operand.remove(i + 1);
+                List_operand.remove(i);
+                List_operand.add(i, result);
+
+                List_operator.remove(i);
+                i--;
+                
             }
         }
 
-        return result;
-    }
 
+        
+      
+
+
+        
+        /**
+         *  Resolve all addition and subtraction operations first from left to right.
+         * Iterates through the operator list and finds + and - operators,
+         * computes the result, and updates both lists accordingly.
+         */
+        for (int i = 0; i < List_operator.size(); i++) {
+            if (List_operator.get(i).equals("+") || List_operator.get(i).equals("-")) {
+                double left = List_operand.get(i);
+                double right = List_operand.get(i + 1);
+                double result ;
+                if(List_operator.get(i).equals("+")){
+                    result = left + right;
+                }else{
+                    result = left - right;
+                }
+
+                List_operand.remove(i + 1);
+                List_operand.remove(i);
+                List_operand.add(i, result);
+
+                List_operator.remove(i);
+                i--;
+                
+            }
+        }
+
+
+           
+      
+
+        /**
+         * @return
+         * returns value of linkedlinked list in double
+         */
+        return List_operand ;
+
+    }
 }
